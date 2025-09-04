4837ad82284c6b4c15572b670fe609c6c6078626
Christoph Welcz <christoph.welcz@codecentric.de>
2025-09-04 13:44:48 +0200

    📝 Add BlueSky profile links

--- components/MyVCard.vue
+++ components/MyVCard.vue
@@ -1,3 +1,3 @@
 <template>
-  <v-card github-url="https://github.com/enolive/pbt-in-real-life"/>
+  <v-card github-url="https://github.com/enolive/pbt-in-real-life" bluesky="chriswelcz"/>
 </template>
